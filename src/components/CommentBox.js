da0f05c7a91cd2ad23f9025e035735f9bbbf32bb
Oscar Villarroel <oscar.villarroel@clouddistrict.com>
2019-03-29 15:05:13 +0100

    changes for pretty pretty home

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default () => {
-  return <div>comment box</div>;
+  const [comment, setComment] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>Add a Comment</h4>
+      <textarea value={comment} onChange={e => setComment(e.target.value)} />
+      <div>
+        <button>Submit</button>
+      </div>
+    </form>
+  );
 };
